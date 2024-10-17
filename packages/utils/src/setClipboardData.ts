278e7c66c9d1688403fbe0e46151a22f7d14e1e0
kassadin <bing.zhi@autostreets.com>
2024-10-17 10:55:30 +0800

    feat: isBrowser

--- packages/utils/src/setClipboardData.ts
+++ packages/utils/src/setClipboardData.ts
@@ -1,17 +1,21 @@
+import { isBrowser } from './isBrowser'
+
 /**
  * 复制到剪切板
  */
-export const setClipboardData = (function () {
-  if (navigator.clipboard) {
-    return (text: string) => navigator.clipboard.writeText(text)
-  } else {
-    return (text: string) => {
-      const input = document.createElement('input')
-      input.value = text
-      document.body.appendChild(input)
-      input.select()
-      document.execCommand('Copy')
-      document.body.removeChild(input)
-    }
-  }
-})()
+export const setClipboardData = isBrowser
+  ? (function () {
+      if (navigator.clipboard) {
+        return (text: string) => navigator.clipboard.writeText(text)
+      } else {
+        return (text: string) => {
+          const input = document.createElement('input')
+          input.value = text
+          document.body.appendChild(input)
+          input.select()
+          document.execCommand('Copy')
+          document.body.removeChild(input)
+        }
+      }
+    })()
+  : () => {}

76af4012fa086dff6c89e2f815e70b78d92af3d7
kassadin <369642339@qq.com>
2025-07-28 09:39:02 +0800

    feat: 按钮显示逻辑

--- playground/src/views/apply/ApplyDetailView.vue
+++ playground/src/views/apply/ApplyDetailView.vue
@@ -50,8 +50,28 @@
           ['init', 'submit', 'withdraw'].includes(computedApplyDetail?.status)
         "
       >
-        <VanButton class="c-button" type="danger" @click="doApplyRemind">催促</VanButton>
-        <VanButton class="c-button" type="warning" @click="doApplyRemind">撤回</VanButton>
+        <VanButton
+          v-if="['withdraw'].includes(computedApplyDetail.submitStatus)"
+          class="c-button"
+          type="primary"
+          @click="handleEdit"
+          >编辑</VanButton
+        >
+
+        <VanButton
+          v-if="['submit'].includes(computedApplyDetail.submitStatus)"
+          class="c-button"
+          type="danger"
+          @click="doApplyRemind"
+          >催促</VanButton
+        >
+        <VanButton
+          v-if="['submit'].includes(computedApplyDetail.submitStatus)"
+          class="c-button"
+          type="warning"
+          @click="doApplyRemind"
+          >撤回</VanButton
+        >
       </HorFixedActions>
     </template>
   </HorView>
@@ -149,6 +169,10 @@
     // 若找到则返回该索引，否则返回最后一个元素的索引
     return index !== -1 ? index : data.value.logs.length - 1
   })
+
+  const handleEdit = () => {
+    console.log('edit')
+  }
 </script>
 
 <style lang="scss" scoped>
